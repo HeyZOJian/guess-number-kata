2299e6051dfb7e6c8dd590a69a51459baf3ea93a
vito zhuang <vito.z.j.zhuang@oocl.com>
2018-07-12 15:33:35 +0800

    The totally wrong test passed

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -2,16 +2,37 @@ package tw.core;
 
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import tw.core.exception.AnswerFormatIncorrectException;
+import tw.core.exception.OutOfGuessCountException;
+import tw.core.generator.AnswerGenerator;
+import tw.core.model.GuessResult;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by jxzhong on 2017/9/23.
  */
 public class AnswerTest {
-    private Answer actualAnswer;
+	private Answer actualAnswer;
+	private Game game;
 
-    @BeforeEach
-    public void setUp() {
-        actualAnswer = Answer.createAnswer("1 2 3 4");
-    }
+	@BeforeEach
+	public void setUp() throws AnswerFormatIncorrectException {
+		actualAnswer = Answer.createAnswer("1 2 3 4");
+		AnswerGenerator answerGenerator = mock(AnswerGenerator.class);
+		when(answerGenerator.generate()).thenReturn(actualAnswer);
+		game = new Game(answerGenerator);
+	}
+
+	@Test
+	public void should_return_0A0B_when_given_answer_are_totally_wrong() throws OutOfGuessCountException {
+		GuessResult guess = game.guess(Answer.createAnswer("5 6 7 8"));
+
+		assertThat(guess.getResult(), is("0A0B"));
+	}
 
 }
